c89466f621af95911391532842cc404a03bc2b0a
yousseflotfy <youssef.medhat@student.guc.edu.eg>
2019-11-28 14:00:41 +0200

    intial landing page

--- moviepal/src/App.js
+++ moviepal/src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+             
+             <div className="center">
+             <h1 className = "title">Welcome to MoviePal</h1>
+             <TextField id="standard-basic" label="Enter your favorite movie to explore your new favorite !" className ="text"  />
+             <div className = "margin">
+             <Button variant="contained" color="primary" >Go !</Button>
+             </div>
+             </div>    
       </header>
     </div>
   );
